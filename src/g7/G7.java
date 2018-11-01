af76d3306a66ac2f00016aae025727e19cd05e0b
Tarc <Tarc@B107>
2018-11-01 10:48:19 +0800

    gear gayyyyyy

--- src/g7/G7.java
+++ src/g7/G7.java
@@ -18,7 +18,7 @@ public class G7 {
         System.out.println("Hello World !");
         System.out.println("Moshi Moshi !");
         System.out.println("Hiiiiiiiiii !");
-
+        System.out.println("Byeeeeeeeee !");
     }
 
 }
